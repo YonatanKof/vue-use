a6f6e0bb3387e2f04a680ef2942d47ad1fa7a138
Yonatan Kof <kof@yonatankof.com>
2023-03-22 22:23:17 +0200

    Add useTitle

---
+++ src/components/useTitle.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useTitle } from "@vueuse/core";
+
+const thisTitle = useTitle();
+
+console.log(thisTitle.value);
+
+thisTitle.value = "Kof tov";
+
+</script>
+
+<template>
+	<h1>Yo</h1>
+	<input type="text" v-model="thisTitle" />
+</template>
