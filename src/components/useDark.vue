ae79d079ada2d59381e9ba2ced39cdc066d40342
Yonatan Kof <kof@yonatankof.com>
2023-03-23 10:58:20 +0200

    Add useDark and useColorMode

---
+++ src/components/useDark.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+	{{ isDark }}
+	<button @click="toggleDark()">Toggle me</button>
+</template>
+<style>
+.dark {
+	background-color: rgb(21, 20, 20);
+	color: antiquewhite;
+}
+</style>
---
+++ src/components/useDarkColorMode.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { useDark, useToggle, useColorMode } from "@vueuse/core";
+const isDark = useDark(); // true or false
+const colorMode = useColorMode({
+	modes: {
+		dim: "dim",
+		cafe: "cafe",
+	},
+	attribute: "theme",
+}); // 'light' or 'dark'
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+	<div>
+		<pre>{{ colorMode }}</pre>
+		<span>
+			<button @click="colorMode = 'dark'">Dark Mode</button>
+			<button @click="colorMode = 'light'">Light Mode</button>
+			<button @click="colorMode = 'dim'">Dim Mode</button>
+			<button @click="colorMode = 'cafe'">Cafe Mode</button>
+		</span>
+	</div>
+</template>
+<style>
+
+[theme="dark"] {
+	background: #252525;
+	color: white;
+}
+[theme="dim"] {
+	background: gray;
+	color: white;
+}
+[theme="cafe"] {
+	background: #c0acac;
+	color: black;
+}
+</style>
