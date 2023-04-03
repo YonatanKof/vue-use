cd8ba601eb0a063c7b1fedc80f131529c14476d3
Yonatan Kof <kof@yonatankof.com>
2023-04-03 19:31:47 +0300

    Add shift+control modifier

--- src/components/onKeyStroke.vue
+++ src/components/onKeyStroke.vue
@@ -1,14 +1,13 @@
 <script setup>
-
-import { ref, watch } from "vue";
+import { ref } from "vue";
 import { onKeyStroke, useMagicKeys, whenever } from "@vueuse/core";
 
 const keys = useMagicKeys();
 const controls = {
-	ArrowDown: () => (position.value.y += 5),
-	ArrowUp: () => (position.value.y -= 5),
-	ArrowRight: () => (position.value.x += 5),
-	ArrowLeft: () => (position.value.x -= 5),
+	ArrowDown: () => (position.value.y += 1),
+	ArrowUp: () => (position.value.y -= 1),
+	ArrowRight: () => (position.value.x += 1),
+	ArrowLeft: () => (position.value.x -= 1),
 };
 
 onKeyStroke(Object.keys(controls), (e) => {
@@ -16,21 +15,39 @@ onKeyStroke(Object.keys(controls), (e) => {
 	controls[e.key]();
 });
 
+/**
+ * * Add shift modifier 
+ */
 whenever(keys.shift_ArrowDown, () => {
-	position.value.y += 50;
+	position.value.y += 10;
 });
 whenever(keys.shift_ArrowUp, () => {
-	position.value.y -= 50;
+	position.value.y -= 10;
 });
 whenever(keys.shift_ArrowRight, () => {
-	position.value.x += 50;
+	position.value.x += 10;
 });
 whenever(keys.shift_ArrowLeft, () => {
-	position.value.x -= 50;
+	position.value.x -= 10;
+});
+
+/**
+ * * Add shift+control modifier 
+ */
+whenever(keys.shift_control_ArrowDown, () => {
+	position.value.y += 100;
+});
+whenever(keys.shift_control_ArrowUp, () => {
+	position.value.y -= 100;
+});
+whenever(keys.shift_control_ArrowRight, () => {
+	position.value.x += 100;
+});
+whenever(keys.shift_control_ArrowLeft, () => {
+	position.value.x -= 100;
 });
 
 const position = ref({ x: 0, y: 0 });
-
 </script>
 
 <template>
@@ -52,15 +69,15 @@ const position = ref({ x: 0, y: 0 });
 <!-- onKeyStroke(["ArrowDown", "ArrowUp", "ArrowRight", "ArrowLeft", shiftArrowDown], (e) => {
 	e.preventDefault();
 	if (e.key === "ArrowDown") {
-		position.value.y += 5;
+		position.value.y += 1;
 	}
 	if (e.key === "ArrowUp") {
-		position.value.y -= 5;
+		position.value.y -= 1;
 	}
 	if (e.key === "ArrowRight") {
-		position.value.x += 5;
+		position.value.x += 1;
 	}
 	if (e.key === "ArrowLeft") {
-		position.value.x -= 5;
+		position.value.x -= 1;
 	}
 }); -->
