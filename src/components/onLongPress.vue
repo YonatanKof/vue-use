273bb3d6793883913a9bc1d4942711b1068f20ea
Yonatan Kof <kof@yonatankof.com>
2023-05-14 10:52:48 +0300

    Add onLongPress

---
+++ src/components/onLongPress.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+import { onLongPress } from '@vueuse/core';
+const longBtn = ref(null);
+onLongPress(
+	longBtn,
+	() => {
+		alert('Sooooo long');
+	},
+	{ delay: 700 }
+);
+</script>
+
+<template>
+    <h3>Press and hold button to click</h3>
+	<button ref="longBtn">Press long</button>
+</template>
--- src/views/use-mouse-and-touch.vue
+++ src/views/use-mouse-and-touch.vue
@@ -1,6 +1,7 @@
 <script setup>
 import useMouseAndTouchDraw from "../components/useMouseAndTouchDraw.vue";
 import useMouseAndTouch from "../components/useMouseAndTouch.vue";
+import onLongPress from "../components/onLongPress.vue";
 </script>
 <template>
     <h1>Mouse & touch</h1>
@@ -11,6 +12,7 @@ import useMouseAndTouch from "../components/useMouseAndTouch.vue";
         <li><code><a href="https://vueuse.org/shared/whenever/#whenever" target="_blank">whenever</a></code></li>
         <li>Vue's <code><a href="https://vuejs.org/guide/essentials/watchers.html#watchers" target="_blank">unwatch</a></code></li>
     </ul>
+	<onLongPress />
 	<useMouseAndTouch />
     <h2>useMouseAndTouchDraw</h2>
 	<useMouseAndTouchDraw />
